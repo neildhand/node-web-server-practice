3791cec1d1ab5232a31e169714e4b34c7ca1c3fd
Neil Dhand <neil_dhand@baylor.edu>
2018-10-11 18:30:40 -0500

    handlebars, rendering templates

--- server.js
+++ server.js
@@ -3,19 +3,39 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
 app.use(express.static(__dirname + '/public'));
 
+app.use( (req, res, next) => {
+    
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Neil',
-        likes: ['basketball', 'football,', 'food']
+    // res.send({
+    //     name: 'Neil',
+    //     likes: ['basketball', 'football,', 'food']
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'WELCOME TO MY WEBSITE!'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    //res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (req, res) => {
